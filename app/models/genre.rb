c942837a88786d141aa035eb47b390b610dc2c3b
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-30 22:00:58 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,8 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.all.each do |t|
-      t.name
-    end
+    self.artists.map |t|
   end
 end
